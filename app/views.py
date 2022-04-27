db9a918d1ad4df7f757c94da0cac8ccba97c85ff
IanoSang <ianosang18@gmail.com>
2022-04-27 13:56:18 +0300

    feat: views- adds a funtion to return the movies page and its data

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_movies
+from .request import get_movies, get_movie
 
 
 # Views
@@ -20,10 +20,12 @@ def index():
                            now_showing=now_showing_movie)
 
 
-@app.route('/movie/<movie_id>')
-def movie(movie_id):
-    '''
+@app.route('/movie/<int:id>')
+def movie(id):
+    """
     View movie page function that returns the movie details page and its data
-    '''
-    title = f'You are viewing {movie_id}'
-    return render_template('movie.html', title=title)
+    """
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html', title=title, movie=movie)
