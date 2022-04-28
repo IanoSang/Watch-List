e4ea8f4d8bd1e8ecf0b312abe83b63bc8efabae8
IanoSang <ianosang18@gmail.com>
2022-04-29 01:32:38 +0300

    feat: revies- adds a form to allow users review the movies on the web app

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import input_required
 
 
 class ReviewForm(FlaskForm):
-    title = StringField('Review title', validators=[Required()])
-    review = TextAreaField('Movie review', validators=[Required()])
+    title = StringField('Review title', validators=[input_required()])
+    review = TextAreaField('Movie review', validators=[input_required()])
     submit = SubmitField('Submit')
